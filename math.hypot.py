c44ced95742baed1855a15baee8f83b7c0f29cec
Sonic <142159825+sonicc97@users.noreply.github.com>
2023-12-07 17:49:08 +0100

    math.hypot.py

--- math.hypot.py
+++ math.hypot.py
@@ -5,3 +5,11 @@ def hypotenuse():
     c = math.hypot(a,b)
     print(f'The length of the hypotenuse is {c}')
 hypotenuse()
+
+#SQUARE ROOT
+import math
+def root():
+    check = float(input('Enter the root:'))
+    number_check = math.sqrt(check)
+    print(f'The square root of a number {check} is {number_check}')
+root()
